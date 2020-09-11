e641c7e7be7cc52af2cd004eddc8354a80829a8f
unknown <tommyconner96@gmail.com>
2020-09-10 21:25:49 -0400

    sliding window better solution -- still not passing large input test though

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,17 +3,16 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # base cases
     if n < 0:
         return 0
     if n == 0:
         return 1
-    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
-        
+    
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 3
+    num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
 
@@ -29,9 +28,6 @@ if __name__ == "__main__":
 # so eating_cookes(3) should return 4
 # there is a large input test and a small input test. the large one may require more
 # optimization
-# from readme:
-# Think about base cases that we would want our recursive function to stop recursing on. 
-# How many ways are there to eat 0 cookies? What about a negative number of cookies? 
 
 # PLAN
 
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,20 +3,29 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 
-def sliding_window_max(nums, k):
-    # array for maximum numbers
-    maximums = []
-    for i in range(len(nums) - k + 1):
-        # loop thru the nums array and each time, update the array of the elements in the window
-        window = []
-        for num in range(k):
-            window.append(nums[i + num])
-        # set maximum for each element in window array
-        maximum = max(window)
-        # append it to maximums array and return it
-        maximums.append(maximum)
-    return maximums
+# def sliding_window_max(nums, k):
+#     # array for maximum numbers
+#     maximums = []
+#     for i in range(len(nums) - k + 1):
+#         # loop thru the nums array and each time, update the array of the elements in the window
+#         window = []
+#         for num in range(k):
+#             window.append(nums[i + num])
+#         # set maximum for each element in window array
+#         maximum = max(window)
+#         # append it to maximums array and return it
+#         maximums.append(maximum)
+#     return maximums
 
+
+def sliding_window_max(nums, k):
+    current = 0
+    maximums = []
+
+    while k + current <= (len(nums)):
+        maximums.append(max(nums[current:k+current]))
+        current += 1
+    return maximums
 
 
 if __name__ == '__main__':
@@ -24,7 +33,8 @@ if __name__ == '__main__':
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
 
 # UNDERSTAND
 # there is a sliding window of a size which is represented by k
